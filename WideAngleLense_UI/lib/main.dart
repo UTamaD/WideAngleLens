b7d57d8457882cd1775bc4cafd8cac4e7e9816bc
jonghyunBaik <pigmammam4@gmail.com>
2023-05-21 17:31:23 +0900

    ui 수정

--- WideAngleLense_UI/lib/main.dart
+++ WideAngleLense_UI/lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -17,15 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wide Angle Detect',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Wide Angle Detect'),
@@ -35,18 +29,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -54,45 +37,50 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   var image = null;
   var loading = false;
+  late List<dynamic> _output;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+  }
+
+  Future<void> analyzeImage() async {
+  }
+
+  void dispose() {
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title, style: TextStyle(color: Colors.black),),
-          backgroundColor: Colors.brown,
+          backgroundColor: Colors.black,
+          toolbarHeight: 10.0,
         ),
         body: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
             children: [
               Column(
-                // Column is also a layout widget. It takes a list of children and
-                // arranges them vertically. By default, it sizes itself to fit its
-                // children horizontally, and tries to be as tall as its parent.
-                //
-                // Invoke "debug painting" (press "p" in the console, choose the
-                // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                // to see the wireframe for each widget.
-                //
-                // Column has various properties to control how it sizes itself and
-                // how it positions its children. Here we use mainAxisAlignment to
-                // center the children vertically; the main axis here is the vertical
-                // axis because Columns are vertical (the cross axis would be
-                // horizontal).
                 children: <Widget>[
                   const SizedBox(
                     height: 20,
                   ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text('Wide Angle Lens',
+                        style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.1, fontWeight: FontWeight.bold),
+                        ),
+                        Text('checker',
+                          style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.06,),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10,),
                   Container(
                     width: MediaQuery.of(context).size.width * 0.9,
                     height: MediaQuery.of(context).size.width * 0.9,
@@ -110,13 +98,17 @@ class _MyHomePageState extends State<MyHomePage> {
                           ],
                         ),
                   ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
                   image != null
-                      ? loading ? Text('normal Lens', style: TextStyle(fontSize: 20, color: Colors.blue)) : Text('Analyzing...', style: TextStyle(fontSize: 20, color: Colors.blue) ,)
+                      ? loading ? Text('normal Lens', style: TextStyle(fontSize: 40, color: Colors.green)) :
+                      Text('Analyzing...', style: TextStyle(fontSize: 40, color: Color.fromRGBO(118, 12, 12, 1)) ,)
                       : const SizedBox(
                           height: 20,
                         ),
                   SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.3,
+                    height: MediaQuery.of(context).size.height * 0.04,
                   ),
                   TextButton(
                       onPressed: () async {
@@ -124,10 +116,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         image =
                             await picker.pickImage(source: ImageSource.gallery);
                             print(image.path);
+                        await analyzeImage();
                         if (image != null) {
                           setState(() {loading = false;});
                           Timer(Duration(seconds: 2), () => { 
-                            setState((){
+                            setState(() {
                               loading = true;
                             })
                           });
@@ -136,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: Container(
                         width: 200,
                         decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
-                        color: Colors.lightBlueAccent,),
+                        color: Colors.black45,),
                         padding: EdgeInsets.all(10.0),
                         child: const Text(
                           'Choose Image',
